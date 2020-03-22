88be856c3f66864efc2711aa9508c37e6c9f7f80
skdGT <skidru00@gmail.com>
2020-03-22 14:32:11 +0000

    added prob4-ex02

---
+++ prob4/ex02/point_c.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+/*
+  When SIGTERM handler is activated, the sigint_handler is interrupted
+*/
+
+
+void sigint_handler(int signo) {
+  printf("Entering SIGINT handler ...\n");
+  sleep(10);
+  printf("Exiting SIGINT handler ...\n");
+}
+
+void sigterm_handler(int signo) {
+  printf("Entering SIGTERM handler ...\n");
+  printf("Exiting SIGTERM handler ...\n");
+}
+
+int main(void) {
+  struct sigaction action;
+  action.sa_handler = sigint_handler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+  if (sigaction(SIGINT,&action,NULL) < 0) {
+    fprintf(stderr,"Unable to install SIGINT handler\n");
+    exit(1);
+  }
+
+  struct sigaction action2;
+  action2.sa_handler = sigterm_handler;
+  sigemptyset(&action2.sa_mask);
+  action2.sa_flags = 0;
+  if (sigaction(SIGTERM, &action2, NULL) < 0) {
+    fprintf(stderr,"Unable to install SIGTERM handler\n");
+    exit(1);
+  }
+
+  printf("Try me with CTRL-C ...\n");
+  while(1) pause();
+  exit(0);
+}

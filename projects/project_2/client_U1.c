82023f22d4305b009d95756cf9ef4cbaa54f133c
skdGT <skidru00@gmail.com>
2020-04-20 11:37:46 +0100

    thread working

--- projects/project_2/client_U1.c
+++ projects/project_2/client_U1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <sys/syscall.h>
 
 #include "utils.h"
+
+
+void* thr_function(void* arg) {
+    pid_t tid;
+    tid = syscall(SYS_gettid);  /* pode ser detetado erro com clang mas compila sem erros */
+
+    log_message((pid_t) tid, 0, 0, 0, (char*) arg);
+    return NULL;
+}
 
 int main(int argc, char** argv) {
     if (argc != 4) {
@@ -11,9 +22,15 @@ int main(int argc, char** argv) {
 
     client_args_t args = parse_client_args(argc, argv);
 
+    long int timeout = args.seconds * 1000 * 1000;
+    long int time = 0;
 
-    printf("Seconds: %d\n", args.seconds);
-    printf("Server: %s\n", args.server_fifo);
+    while (time < timeout) {
+        pthread_t tid;
+        pthread_create(&tid, NULL, thr_function, "Hi there");
+        usleep(500);
+        time += 500;
+    }
 
     exit(0);
 }
